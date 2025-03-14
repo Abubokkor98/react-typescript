ea0051013ace4be5cc27f90da23d41aff54241ff
Abubokkor98 <mail.abubokkor@gmail.com>
2025-03-14 23:15:33 +0600

    user defined Props types (part-1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
 import "./App.css";
 import User from "./components/User";
 
+const user1 = {
+  name: "Abu Bokkor Siddik",
+  age: 27,
+  isVerified: false,
+  lang: ["english", " bangla"],
+};
+const user2 = {
+  name: "Sumaiya Akter",
+  age: 23,
+  isVerified: true,
+  lang: ["english", " bangla"],
+};
+
 function App() {
   return (
     <>
       <h1>React Typescript</h1>
-      <User name={"Abu Bokkor Siddik"} age={27} isVerified={false} />
-      <User name={"Sumaiya Akter"} age={23} isVerified={true} />
-
+      {/* <User
+        name={"Abu Bokkor Siddik"}
+        age={27}
+        isVerified={false}
+        lang={["english", " bangla"]}
+      /> */}
+      <User user={user1} />
+      <User user={user2} />
     </>
   );
 }
--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,17 +1,30 @@
+// build in types: string, number, boolean, void, null
+// user defined types: object, array, enum, union, any, custom type
+
 // for better props
 type UserProps = {
-  name: string;
-  age: number;
-  isVerified: boolean;
+  user: {
+    name: string;
+    age: number;
+    isVerified: boolean;
+    lang: string[];
+  };
 };
 
 export default function User(props: UserProps) {
   return (
     <div>
       <h2>User</h2>
-      <h3>{props.name}</h3>
-      <p>{props.age} years old</p>
-      {props.isVerified ? <p>Verified User </p> : <p> unVerified user </p>}
+      <h3>{props.user.name}</h3>
+      <p>{props.user.age} years old</p>
+      {props.user.isVerified ? <p>Verified User </p> : <p> unVerified user </p>}
+      {/* <p>{props.lang}</p> */}
+      <p>
+        Speaks:{" "}
+        {props.user.lang.map((language, index) => (
+          <span key={index}>{language}</span>
+        ))}
+      </p>
     </div>
   );
 }
